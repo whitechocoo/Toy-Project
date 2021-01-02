cbacd0707d1eeeb121642872090bdb9b29afd63a
dsunni <dongsunlee00@gmail.com>
2021-01-02 23:11:40 +0900

    [ADD] 스프링 핵심 원리 실습 CORE

--- 스프링핵심원리_실습/core/src/main/java/hello/core/MemberApp.java
+++ 스프링핵심원리_실습/core/src/main/java/hello/core/MemberApp.java
@@ -7,9 +7,12 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
+
         Member findMember = memberService.findMember(1L);
         System.out.println("new Member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
---
+++ 스프링핵심원리_실습/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 함")
+    void vip_o() {
+        // given
+        Member memberVIP = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(memberVIP, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 함")
+    void vip_x() {
+        // given
+        Member memberBasic = new Member(1L, "memberBasic", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(memberBasic, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
